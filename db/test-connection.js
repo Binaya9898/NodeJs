211d131cfae9c6f33b519d4e9739cd33003df329
Binaya KOirala <beeenaya98@gmail.com>
2023-05-05 17:37:25 +0545

    mongodb

---
+++ db/test-connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    // "mongodb+srv://cfc-demo-user:asdfasdf1@cluster0.i35rvgk.mongodb.net/cfc-db",
+    "mongodb://127.0.0.1:27017/db-name"
+    ,
+  )
+  .then(function () {
+    console.log("Connected!!");
+  })
+  .catch(function (e) {
+    console.log("Error connecting: " + e.toString());
+  });
