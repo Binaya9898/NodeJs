5241b3aea78d2632680b5a06504d502289e8802d
Binaya KOirala <beeenaya98@gmail.com>
2023-05-05 11:51:35 +0545

    server done

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-var vehicle=[car={
-    color:"blue",
-    speed:"105",
-    brand:"tesla"
-},
-
-bus={
-    color:"green",
-    speed:"102",
-    brand:"tata"
-},
-
-]
-console.log(vehicle.map(car.speed));
+const express= require("express");
+// console.log(express);
+const httpServer=express();
+httpServer.get("/",function(req,res){
+    console.log("Request Found");
+    res.send("I am BInaya");
+})
+httpServer.listen(5000,function(){
+    console.log("Server listening on port 5000")
+});
