257abea6985ba73b3d891f5fcc47a5295579348f
Binaya KOirala <beeenaya98@gmail.com>
2023-05-05 11:35:48 +0545

    Node Camp

---
+++ script.js
@@ -0,0 +1,14 @@
+var vehicle=[car={
+    color:"blue",
+    speed:"105",
+    brand:"tesla"
+},
+
+bus={
+    color:"green",
+    speed:"102",
+    brand:"tata"
+},
+
+]
+console.log(vehicle.map(car.speed));
