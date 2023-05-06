8ceb29c6d445e57691d650bf072ad6aa9ad765c4
Binaya KOirala <beeenaya98@gmail.com>
2023-05-06 11:57:22 +0545

    post created

--- routes.js
+++ routes.js
@@ -1,18 +1,37 @@
 const express=require("express");
+const createNewPost = require("./function");
 const router =express.Router();
+// sending params
+// router.get("/hi",function(req,res){
+//     console.log("Request Found");
+//     if(req.query.name !=undefined){
+//         res.send("Hi, "+ req.query.name);
+//     }
+//     else 
+//   {  res.send("Hello");}
+// })
+// router.post("/bye",function(req,res){
+//     console.log(req.body);
+//     res.send("BYE");
+// })
 
-router.get("/hi",function(req,res){
-    console.log("Request Found");
-    res.send("HI");
-})
-router.get("/bye",function(req,res){
-    console.log("Request Found");
-    res.send("BYE");
-})
+// router.get("/byebye",function(req,res){
+//     console.log(req.body.height); 
+//     res.send("Hello from post request");
+// })
 
-router.post("/",function(req,res){
-    console.log("Request Found");
-    res.send("Hello from post request");
+router.post("/",(req,res)=>{
+    createNewPost(req.body.postContent, req.body.postCreatedBy)
+    .then(function (post) {
+      res.send("Post Created successfully");
+    })
+    .catch(function (error) {
+      res.send("Error Creating Post");
+    });
+
+
+
+
 })
 
 module.exports=router;
--- script.js
+++ script.js
@@ -2,7 +2,25 @@ const express= require("express");
 const routes=require("./routes")
 // console.log(express);
 const httpServer=express();
+httpServer.use(express.json());
 httpServer.use(routes);
-httpServer.listen(5000,function(){
-    console.log("Server listening on dnjd port 5000")
-});
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    // "mongodb+srv://cfc-demo-user:asdfasdf1@cluster0.i35rvgk.mongodb.net/cfc-db",
+    "mongodb://127.0.0.1:27017/db-name"
+    ,
+  )
+  .then(function () {
+    httpServer.listen(5000,function(){
+        console.log("Server listening on dnjd port 5000")
+    });  })
+  .catch(function (e) {
+    console.log("Error connecting: " + e.toString());
+  });
+
+// httpServer.listen(5000,function(){
+//     console.log("Server listening on dnjd port 5000")
+// });
